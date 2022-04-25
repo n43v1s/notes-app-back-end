c37aea1a3e28ab1249705403e0a87781113a5e94
n43v1s <agusrokyanto@gmail.com>
2022-04-25 23:39:33 +0700

    restructure with hapi plugin

--- src/notes.js
+++
@@ -1,6 +0,0 @@
-/*eslint linebreak-style: ["error", "windows"]*/
-/*eslint spaced-comment: ["error", "never", { "markers": ["!<"] }]*/
-
-const notes = [];
-
-module.exports = notes;
--- src/server.js
+++ src/server.js
@@ -1,24 +1,29 @@
-/*eslint linebreak-style: ["error", "windows"]*/
-/*eslint indent: "error"*/
-/*eslint spaced-comment: ["error", "never", { "markers": ["!<"] }]*/
-
 const Hapi = require('@hapi/hapi');
-const routes = require('./routes');
+const notes = require('./api/notes');
+const NotesService = require('./services/inMemory/NotesService');
 
 const init = async () => {
-    const server = Hapi.server({
-        port: 5000,
-        host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
-        routes: {
-            cors: {
-                origin: ['*'],
-            },
-        },
-    });
-    server.route(routes);
+  const notesService = new NotesService();
 
-    await server.start();
-    console.log(`Server berjalan pada ${server.info.uri}`);
+  const server = Hapi.server({
+    port: 5000,
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  await server.register({
+    plugin: notes,
+    options: {
+      service: notesService,
+    },
+  });
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
 };
 
 init();
